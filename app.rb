a11c72195e802b8e66aabea9fa054b4de0ceae4b
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-02-24 16:47:04 -0500

    commit tree c19b46812c62acbf6f1147be10a6e2202c6e1b05

--- app.rb
+++ app.rb
@@ -1,6 +1,52 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    
+    #makes the form
+    get '/newteam' do
+      erb :newteam
+    end
 
-
+    post '/newteam' do
+      @newteam = params
+      erb :team
+    end
+  
 end
+
+
+#1
+# post '/reverse' do
+#   original_string = params["string"]
+#   @reversed_string = original_string.reverse
+
+#   erb :reversed
+# end 
+
+#2
+# get "/" do
+#   @user = "Ian"
+
+#   erb :index # @user will be defined as 'Ian' in the view
+# end
+
+# get "/profile" do
+#   erb :profile # @user will be nil here
+# end 
+
+# #3
+# <!DOCTYPE html>
+# <html>
+#   <head>
+#     <meta charset="UTF-8">
+#     <title>The Ultimate Reversed String!</title>
+#     <link rel="stylesheet" href="stylesheets/style.css" type="text/css">
+#   </head>
+
+#   <body>
+#     <h1>The Ultimate Reversed String!</h1>
+
+#     <!--   Use ERB tags to bring in an instance variable containing the reversed string -->
+#     <h2><%= @reversed_string %></h2>
+#   </body>
+# </html> 
